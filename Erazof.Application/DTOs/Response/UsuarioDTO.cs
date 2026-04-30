c9b38bb4528049166f4426a3baf8f0ec35c830b5
erazof <brayan.erazof@gmail.com>
2026-04-30 12:29:14 -0500

    feat: se concluto el api

---
+++ Erazof.Application/DTOs/Response/UsuarioDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Erazof.Application.DTOs.Response
+{
+    public class UsuarioDTO
+    {
+        public int idUser { get; set; }
+        public string usuario { get; set; }
+        public string email { get; set; }
+        public DateTime fechaReg { get; set; }
+    }
+}
---
+++ Erazof.WebApp/Controllers/UsuariosApiController.cs
@@ -0,0 +1,40 @@
+﻿using Erazof.Application.DTOs.Response;
+using Erazof.Application.Interfaces;
+using Erazof.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Erazof.WebApp.Controllers
+{
+    [Route("api/usuarios")]
+    [ApiController]
+    public class UsuariosApiController : ControllerBase
+    {
+        private readonly IUsuarioService _usuarioService;
+        public UsuariosApiController(IUsuarioService usuarioService)
+        {
+            _usuarioService = usuarioService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsuario()
+        {
+            List<UsuarioDTO> listaUserDTO = new List<UsuarioDTO>();
+            var listaUser = await _usuarioService.ListarTodos();
+            foreach(var user in listaUser)
+            {
+                UsuarioDTO userDTO = new UsuarioDTO
+                {
+                    idUser = user.UsuarioID,
+                    usuario = user.Username,
+                    email = user.Email,
+                    fechaReg = user.FechaReg
+                };
+                listaUserDTO.Add(userDTO);
+            }
+
+            return Ok(listaUserDTO);
+        }
+
+    }
+}
